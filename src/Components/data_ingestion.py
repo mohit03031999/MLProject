37f9ad72b439d3fbf95bdf2718960498400a07ff
Mohit <39363730+mohit03031999@users.noreply.github.com>
2024-01-01 13:10:09 -0800

    Main

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -7,37 +7,53 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.Components.data_transformation import DataTransformation
+from src.Components.model_trainer import ModelTrainer
+
+
 @dataclass
-class dataIngestionConfigs:
-    train_data_path: str=os.path.join('artifacts',"train.csv")
-    test_data_path: str=os.path.join('artifacts',"test.csv")
-    raw_data_path: str=os.path.join('artifacts',"data.csv")
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', "train.csv")
+    test_data_path: str = os.path.join('artifacts', "test.csv")
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
 
 
-class dataIngestion:
-    def __int__(self):
-        self.ingestion_conig = dataIngestionConfigs()
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
         logging.info("Enter the data ingestion method or component")
         try:
-            df = pd.read_csv('src/data/stud.csv')  #Raed the dataset
+            df = pd.read_csv('/Users/moagarwal/Documents/ML_Project/src/Data/stud.csv')  # Read the dataset
             logging.info("Read the dataset as DataFrame")
 
-            os.makedirs(os.path.dirname(self.ingestion_conig.train_data_path),exist_ok=True) #Create the directory and if exits keep
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),
+                        exist_ok=True)  # Create the directory and if exits keep
 
-            df.to_csv(self.ingestion_conig.raw_data_path,index=False,header=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
-            train_set,test_set = train_test_split(df,test_size=0.3,random_state=42)
+            train_set, test_set = train_test_split(df, test_size=0.3, random_state=42)
 
-            train_set.to_csv(self.ingestion_conig.train_data_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_conig.test_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Completed the Ingestion of Config")
 
             return (
-                self.ingestion_conig.train_data_path,
-                self.ingestion_conig.test_data_path
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
             )
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
--- src/Components/data_transformation.py
+++ src/Components/data_transformation.py
@@ -15,12 +15,12 @@ from src.utils import save_object
 
 
 @dataclass
-class dataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('artifacts', "preprocessor.pkl")
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
-class dataTransformation:
-    def __int__(self):
-        self.data_transformation_config = dataTransformationConfig()
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
         try:
@@ -64,32 +64,32 @@ class dataTransformation:
 
             return preprocessor
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
 
-    def initate_data_transformtion(self, train_path, test_path):
+    def initiate_data_transformation(self, train_path, test_path):
         try:
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
 
             logging.info("Obtaining preprocessor object")
 
-            preprocessing_obj=self.get_data_transformer_object()
+            preprocessing_obj = self.get_data_transformer_object()
 
-            target_column_name="math_score"
+            target_column_name = "math_score"
             numerical_columns = ["writing_score", "reading_score"]
 
-            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
-            target_feature_train_df=train_df[target_column_name]
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
 
-            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df=test_df[target_column_name]
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
 
             logging.info(
                 f"Applying preprocessing object on training dataframe and testing dataframe."
             )
 
-            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
@@ -111,7 +111,4 @@ class dataTransformation:
                 self.data_transformation_config.preprocessor_obj_file_path,
             )
         except Exception as e:
-            raise CustomException(e,sys)
-
-
-
+            raise CustomException(e, sys)
